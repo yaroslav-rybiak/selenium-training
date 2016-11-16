a42b2b30d3f710f999e22887f7f87446f41d74bf
Yaroslav Rybiak <yaroslav.rybiak@gmail.com>
2016-11-16 12:34:05 +0200

    duck store test

--- src/test/DuckStoreTest.java
+++ src/test/DuckStoreTest.java
@@ -4,28 +4,26 @@ import org.junit.Test;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
-import static org.openqa.selenium.support.ui.ExpectedConditions.titleIs;
-
-public class MyFirstTest {
+public class DuckStoreTest {
 
     private WebDriver driver;
     private WebDriverWait wait;
 
     @Before
     public void start() {
-        driver = new FirefoxDriver();
+        driver = new ChromeDriver();
         wait = new WebDriverWait(driver, 10);
     }
 
     @Test
-    public void myFirstTest() {
-        driver.get("http://www.google.com/ncr");
-        driver.findElement(By.name("q")).sendKeys("webdriver");
-        driver.findElement(By.name("btnG")).click();
-        wait.until(titleIs("webdriver - Google Search"));
+    public void loginAsAdmin() {
+        driver.get("http://localhost/litecart/admin/login.php");
+        driver.findElement(By.name("username")).sendKeys("admin");
+        driver.findElement(By.name("password")).sendKeys("admin");
+        driver.findElement(By.name("login")).click();
     }
 
     @After
