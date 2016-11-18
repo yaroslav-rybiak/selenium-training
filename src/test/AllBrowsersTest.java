d9ba35f2d07b464c1222216e1f98692b6076fedc
Yaroslav Rybiak <yaroslav.rybiak@gmail.com>
2016-11-18 18:41:47 +0200

    generic "before" with info for all browsers

---
+++ src/test/AllBrowsersTest.java
@@ -0,0 +1,80 @@
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.Assert;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.firefox.FirefoxBinary;
+import org.openqa.selenium.firefox.FirefoxProfile;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
+
+import java.io.File;
+
+public class AllBrowsersTest {
+
+    private WebDriver driver;
+    private WebDriverWait wait;
+
+    @Before
+    public void start() {
+
+        //Chrome browser options
+        /*
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("start-maximized");
+        driver = new ChromeDriver(options);
+        */
+
+        //Firefox browser options NEW scheme for standard build
+        /*
+        DesiredCapabilities caps = new DesiredCapabilities();
+        caps.setCapability(FirefoxDriver.MARIONETTE, true);
+        driver = new FirefoxDriver(caps);
+        */
+
+        //Firefox browser options for nightly build
+
+        driver = new FirefoxDriver(new FirefoxBinary(new File("C:\\Program Files\\Nightly\\firefox.exe")),
+                new FirefoxProfile());
+
+        //Firefox browser options OLD scheme for firefox 45
+        /*
+        DesiredCapabilities caps = new DesiredCapabilities();
+        caps.setCapability(FirefoxDriver.MARIONETTE, false);
+        driver = new FirefoxDriver(new FirefoxBinary(new File("C:\\Program Files (x86)\\Mozilla Firefox 45\\firefox.exe")),
+                new FirefoxProfile(), caps);
+        */
+
+        //IE browser options
+        /*
+        DesiredCapabilities caps = new DesiredCapabilities();
+        caps.setCapability(InternetExplorerDriver.IGNORE_ZOOM_SETTING, true);
+        caps.setCapability(InternetExplorerDriver.INTRODUCE_FLAKINESS_BY_IGNORING_SECURITY_DOMAINS, true);
+        driver = new InternetExplorerDriver(caps);
+        */
+
+
+        wait = new WebDriverWait(driver, 10);
+    }
+
+    @Test
+    public void loginAsAdmin() {
+        driver.get("http://localhost/litecart/admin/login.php");
+        driver.findElement(By.name("username")).sendKeys("admin");
+        driver.findElement(By.name("password")).sendKeys("admin");
+        driver.findElement(By.name("login")).click();
+        Assert.assertEquals("http://localhost/litecart/admin/", driver.getCurrentUrl());
+    }
+
+    @After
+    public void stop() {
+        driver.quit();
+        driver = null;
+    }
+}
