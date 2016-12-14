aede2d1f63d8da7cd5d9b62b24ecaeedb5d1bfd1
Yaroslav Rybiak <yaroslav.rybiak@gmail.com>
2016-12-14 16:14:24 +0200

    screenshot

--- src/test/EventFiringTest.java
+++ src/test/EventFiringTest.java
@@ -3,14 +3,16 @@ package test;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
+import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.support.events.AbstractWebDriverEventListener;
 import org.openqa.selenium.support.events.EventFiringWebDriver;
 import org.openqa.selenium.support.ui.WebDriverWait;
+
+import com.google.common.io.Files;
+import java.io.File;
+import java.io.IOException;
 import java.util.concurrent.TimeUnit;
 
 public class EventFiringTest {
@@ -25,11 +27,17 @@ public class EventFiringTest {
             System.out.println(by + " found");
         }
 
+
         @Override
         public void onException(Throwable throwable, WebDriver driver) {
             System.out.println(throwable);
+            File tempFile = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
+            try {
+                Files.copy(tempFile, new File("screen.png"));
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
         }
-
     }
 
 
@@ -54,6 +62,10 @@ public class EventFiringTest {
         driver.get("http://google.com/ncr");
         WebElement searchField = driver.findElement(By.cssSelector("[name=q]"));
         searchField.sendKeys("EventFiringTest");
+
+        //here should be exception screenshot
+        WebElement fail = driver.findElement(By.cssSelector("[name=fail]"));
+        fail.sendKeys("fail");
 
     }
 
